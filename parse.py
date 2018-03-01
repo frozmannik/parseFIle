6d0affbf2ff89e39b92161e0eb00f4004187e626
nick <frozmanik@gmail.com>
2018-03-01 13:06:00 +0200

    parse file

--- parse.py
+++ parse.py
@@ -1,7 +1,6 @@
 import os
 
-file_name = 'example.in'
-
+file_name = 'example.in'  # name of file
 file_path = os.path.abspath(os.path.join('data',file_name))
 f = open(file_path,'r')
 dict = f.readline().split() # read a first line and split value in it
